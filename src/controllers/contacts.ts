fea98b805eafe088d91e344d0ac54fec5b6aa13d
roboli <robertooliveros@mac.com>
2022-11-16 10:06:10 -0600

    Catch any errors if any

--- src/controllers/contacts.ts
+++ src/controllers/contacts.ts
@@ -1,30 +1,44 @@
 import * as contacts from '../models/contacts';
 
 export const getAll = async (req: any, res: any) => {
-  const results = await contacts.getAll();
-  res.json(results);
+  try {
+    const results = await contacts.getAll();
+    res.json(results);
+  } catch (e) {
+    console.error(e);
+    res.sendStatus(500);
+  }
 };
 
 export const getOne = async (req: any, res: any) => {
   const { id } = req.params;
 
-  const result = await contacts.getOne({ id });
-
-  res.json(result);
+  try {
+    const result = await contacts.getOne({ id });
+    res.json(result);
+  } catch (e) {
+    console.error(e);
+    res.sendStatus(500);
+  }
 };
 
 export const create = async (req: any, res: any) => {
   const { body } = req;
 
-  const id = await contacts.create({
-    name: body.name,
-    email: body.email,
-    phone_work: body.phone_work,
-    phone_home: body.phone_home,
-    phone_mobile: body.phone_mobile,
-    phone_other: body.phone_other,
-    address: body.address
-  });
+  try {
+    const id = await contacts.create({
+      name: body.name,
+      email: body.email,
+      phone_work: body.phone_work,
+      phone_home: body.phone_home,
+      phone_mobile: body.phone_mobile,
+      phone_other: body.phone_other,
+      address: body.address
+    });
 
-  res.status(201).json({ id });
+    res.status(201).json({ id });
+  } catch (e) {
+    console.error(e);
+    res.sendStatus(500);
+  }
 }
